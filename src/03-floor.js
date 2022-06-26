3b3ba409f3340d09a9f2ccdb3b8e7923fe285cc2
Johnny <joaogardenberg@gmail.com>
2022-06-26 21:26:07 +0200

    03-floor

--- src/03-floor.js
+++ src/03-floor.js
@@ -13,12 +13,22 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   2000
 )
-camera.position.set(0, 0, 5)
+camera.position.z = 5
 
 const characterGeometry = new THREE.SphereBufferGeometry(2, 32, 32)
 const characterMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
 const characterMesh = new THREE.Mesh(characterGeometry, characterMaterial)
 scene.add(characterMesh)
+
+const floorGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 10, 10)
+const floorMaterial = new THREE.MeshBasicMaterial({
+  color: 0x228b22,
+  side: THREE.DoubleSide
+})
+const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial)
+floorMesh.position.y = -5
+floorMesh.rotation.x = Math.PI / 2
+scene.add(floorMesh)
 
 const animate = () => {
   requestAnimationFrame(animate)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.scss'
 // import './01-simple-scene'
-import './02-character'
+// import './02-character'
+import './03-floor'
